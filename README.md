3678a309a04e2fc7ea0e199b9638a678b3ca7b43
Beshoy Nabeih <beshoybosha231@gmail.com>
2020-12-13 16:13:40 +0200

    auth api

---
+++ README.md
@@ -0,0 +1,21 @@
+# hdya-api
+
+## how to install
+
+
+```bash
+  $ python -m venv .vevn
+  $ source .venv/bin/active
+  $ pip install -r requirments.txt
+```
+
+  Setup database credentials in settings.py
+
+```bash
+  $ python manage.py makemigrations
+  $ python manage.py migrate
+```
+  Finally
+```bash
+  $ python manage.py runserver
+```
---
+++ product/models.py
@@ -0,0 +1,103 @@
+from django.db import models
+from django.utils import timezone
+from authentication.models import User
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Occassion(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+# class ProductOccassion(models.Model):
+#     product_id = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+#     occassion_id = models.ForeignKey(Occassion, on_delete=models.CASCADE, null=True)
+
+class RelationShip(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=45)
+    details = models.TextField(max_length=3000)
+    price = models.IntegerField()
+    age_from = models.IntegerField()
+    age_to = models.IntegerField()
+    gender = models.CharField(
+        null=True,
+        max_length=50,
+        choices=[
+            ('m', 'Male'),
+            ('f', 'female'),
+            ('b', 'both')
+        ])
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    occassions = models.ManyToManyField(Occassion)
+    relationships = models.ManyToManyField(RelationShip)
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductPicture(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    img_url = models.ImageField(upload_to='static/products/images/', verbose_name='Images')
+
+    def __str__(self):
+        return self.product_id.name
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Rate(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rate = models.CharField(
+        null=True,
+        max_length=50,
+        choices=[
+            ('1', '1'),
+            ('2', '2'),
+            ('3', '3'),
+            ('4', '4'),
+            ('5', '5'),
+        ])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ProductReport(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ReviewReport(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
