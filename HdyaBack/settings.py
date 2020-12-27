3fa29f75e1ae60d1231ea8e0c2255efa9243bc84
Beshoy Nabeih <beshoybosha231@gmail.com>
2020-12-27 22:10:59 +0200

    fix images bug

--- HdyaBack/settings.py
+++ HdyaBack/settings.py
@@ -133,11 +133,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, ('staticfiles'))
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "media"),
-]
+MEDIA_URL = 'media/'
+# STATIC_ROOT = os.path.join(BASE_DIR, ('staticfiles'))
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "media"),
+# ]
+MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
--- product/models.py
+++ product/models.py
@@ -58,7 +58,7 @@ class Product(models.Model):
 
 class ProductPicture(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='static/products/images/', verbose_name='Image')
+    image = models.ImageField(upload_to='products/', verbose_name='Image')
 
     def __str__(self):
         return self.product.name
