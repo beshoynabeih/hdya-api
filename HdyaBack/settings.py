d7fb230c4755efc9531d6f478856068b2c2d041b
Beshoy Nabeih <beshoybosha231@gmail.com>
2021-01-04 21:52:47 +0200

    update pagination settings

--- HdyaBack/settings.py
+++ HdyaBack/settings.py
@@ -141,8 +141,8 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 9,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 9,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
